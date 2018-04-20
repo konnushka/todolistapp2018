f31188aeb1ab3740171a04e822e5323c1fdcf7c0
konnushka <konnushka@gmail.com>
2018-04-20 16:37:26 +1000

    seconday class- added delete,complete and sort features

---
+++ Desktop/todolist2018/ts/task.ts
@@ -0,0 +1,11 @@
+class Task{
+
+    id:string;
+    name:string;
+    status:boolean;
+constructor(taskname:string){
+    this.id = new Date().getTime().toString();
+    this.name = taskname;
+    this.status = false;
+    }
+}
